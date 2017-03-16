e6ac3857dfe1762e85457de217264735e0d465fb
GracielaCesareo <graciela.cesareoc@gmail.com>
2017-03-16 10:50:54 -0600

    ejercicios html 01

--- assets/javascript03.js
+++ assets/javascript03.js
@@ -4,8 +4,6 @@ var resultado = parseInt(farenheit) * 1.8 + 32;
 document.getElementById('farenheit').value = resultado;
 }
 
-function convertirCelsius(){
-  return (document.getElementById('celsius').value;
-  var resultado = parseInt((celsius)- 32) * 1.8;
-  document.getElementById('celsius').value = resultado;
+//function convertirCelsius(farenheit){
+  return (document.getElementById('celsius').value - 32) * 1.8;
 }
